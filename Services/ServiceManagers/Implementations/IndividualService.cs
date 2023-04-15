096bb041ce5a982f27c87034a289c1ea2749915e
Chandima Ekanayake <emcc1990@gmail.com>
2023-04-15 20:27:34 +0530

    Added service implementations

---
+++ Services/ServiceManagers/Implementations/IndividualService.cs
@@ -0,0 +1,33 @@
+﻿using InSharpAssessment.Services.Models.ServiceDTOs;
+using InSharpAssessment.Services.ServiceManagers.Abstractions;
+
+namespace InSharpAssessment.Services.ServiceManagers.Implementations
+{
+    public class IndividualService : IIndividualService
+    {
+        public async Task<bool> DeleteIndividualAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<int> CreateIndividualAsync(IndividualCreateServiceDTO individual)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<List<IndividualServiceDTO>> GetAllIndividualsAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<IndividualServiceDTO> GetIndividualByIdAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<int> UpdateIndividualAsync(IndividualUpdateServiceDTO individual)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
