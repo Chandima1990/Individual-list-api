80d5221552adca2223253bb8f34d961d4e7ff411
Chandima Ekanayake <emcc1990@gmail.com>
2023-04-15 20:26:24 +0530

    Added service interfaces

--- Services/ServiceManagers/Abstractions/IAddressService.cs
+++ Services/ServiceManagers/Abstractions/IAddressService.cs
@@ -1,6 +1,42 @@
-﻿namespace Services.ServiceManagers.Abstractions
+﻿using InSharpAssessment.Services.Models.ServiceDTOs;
+
+namespace InSharpAssessment.Services.ServiceManagers.Abstractions
 {
     public interface IAddressService
     {
+        //<summary>
+        // Get Address by Id
+        // </summary>
+        // <param name="id">Address Id</param>
+        // <returns>Address</returns>
+        Task<AddressServiceDTO> GetAddressByIdAsync(int id);
+
+        // <summary>
+        // Create Address for Individual
+        // </summary>
+        // <param name="address">Address</param>
+        // <returns>Address Id</returns>
+        Task<int> CreateAddressAsync(AddressServiceDTO address);
+
+        // <summary>
+        // Update Address for Individual
+        // </summary>
+        // <param name="address">Address</param>
+        // <returns>Address</returns>
+        Task<AddressServiceDTO> UpdateAddressAsync(AddressServiceDTO address);
+
+        // <summary>
+        // Delete Address for Individual
+        // </summary>
+        // <param name="address">Address</param>
+        // <returns>Address Deleted</returns>
+        Task<bool> DeleteAddressAsync(AddressServiceDTO address);
+
+        // <summary>
+        // Get All Addresses for Individual
+        // </summary>
+        // <param name="individualId">Individual Id</param>
+        // <returns>Address</returns>
+        Task<List<AddressServiceDTO>> GetAllAddressesForIndividualAsync(int individualId);
     }
 }
