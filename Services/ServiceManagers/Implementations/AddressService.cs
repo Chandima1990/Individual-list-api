d38ee9ff4c79080ce31c74ec578ca7658db70320
Chandima Ekanayake <emcc1990@gmail.com>
2023-04-15 20:27:34 +0530

    Added service implementations

---
+++ Services/ServiceManagers/Implementations/AddressService.cs
@@ -0,0 +1,33 @@
+﻿using InSharpAssessment.Services.Models.ServiceDTOs;
+using InSharpAssessment.Services.ServiceManagers.Abstractions;
+
+namespace InSharpAssessment.Services.ServiceManagers.Implementations
+{
+    public class AddressService : IAddressService
+    {
+        public async Task<int> CreateAddressAsync(AddressServiceDTO address)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<bool> DeleteAddressAsync(AddressServiceDTO address)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<AddressServiceDTO> GetAddressByIdAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<List<AddressServiceDTO>> GetAllAddressesForIndividualAsync(int individualId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<AddressServiceDTO> UpdateAddressAsync(AddressServiceDTO address)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
