3d6997936d74d6a0d9498b7b6d640f97c55e8a82
Chandima Ekanayake <emcc1990@gmail.com>
2023-04-15 17:23:10 +0530

    Added service DTOs

---
+++ Services/ServiceDTOs/IndividualServiceDTO.cs
@@ -0,0 +1,12 @@
+﻿namespace InSharpAssessment.DataRepositories.Models.DTOs
+{
+    internal class IndividualServiceDTO
+    {
+        public int Id { get; set; }
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public List<AddressServiceDTO>? Addresses { get; set; }
+        public string? PhoneNumber { get; set; }
+        public int AgeInYears { get; set; }
+    }
+}
