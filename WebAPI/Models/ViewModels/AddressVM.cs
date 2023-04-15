1a192be82dd841eec8147e089096c760aed2239d
Chandima Ekanayake <emcc1990@gmail.com>
2023-04-15 20:25:22 +0530

    Added VMs

---
+++ WebAPI/Models/ViewModels/AddressVM.cs
@@ -0,0 +1,25 @@
+﻿namespace InSharpAssessment.WebAPI.Models.ViewModels
+{
+    public class AddressCreateVM
+    {
+        public string? Street { get; set; }
+        public string? City { get; set; }
+        public string? Country { get; set; }
+    }
+
+    public class AddressUpdateVM
+    {
+        public int? Id { get; set; }
+        public string? Street { get; set; }
+        public string? City { get; set; }
+        public string? Country { get; set; }
+    }
+
+    public class AddressVM
+    {
+        public int Id { get; set; }
+        public string? Street { get; set; }
+        public string? City { get; set; }
+        public string? Country { get; set; }
+    }
+}
---
+++ WebAPI/Models/ViewModels/IndividualVM.cs
@@ -0,0 +1,30 @@
+﻿namespace InSharpAssessment.WebAPI.Models.ViewModels
+{
+    public class IndividualCreateVM
+    {
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public List<AddressCreateVM> Addresses { get; set; }
+        public string? PhoneNumber { get; set; }
+        public int AgeInYears { get; set; }
+    }
+
+    public class IndividualUpdateVM
+    {
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public List<AddressUpdateVM> Addresses { get; set; }
+        public string? PhoneNumber { get; set; }
+        public int AgeInYears { get; set; }
+    }
+
+    public class IndividualVM
+    {
+        public int Id { get; set; }
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public List<AddressVM> Addresses { get; set; }
+        public string? PhoneNumber { get; set; }
+        public int AgeInYears { get; set; }
+    }
+}
