854618069d167dce3f6596c99744c156fa390360
Chandima Ekanayake <emcc1990@gmail.com>
2023-04-15 17:24:43 +0530

    Addresses nullable removed

--- DataRepositories/Models/Entities/Individual.cs
+++ DataRepositories/Models/Entities/Individual.cs
@@ -3,9 +3,9 @@
     public class Individual : BaseEntity
     {
         public int Id { get; set; }
-        public string?  FirstName { get; set; }
+        public string? FirstName { get; set; }
         public string? LastName { get; set; }
-        public virtual List<Address>? Addresses { get; set; }
+        public virtual List<Address> Addresses { get; set; }
         public string? PhoneNumber { get; set; }
         public int AgeInYears { get; set; }
 
