31aa318a0cf1fd69ed4f3cb16ef82e83b8f00546
Chandima Ekanayake <emcc1990@gmail.com>
2023-04-15 20:24:53 +0530

    Added validator

---
+++ WebAPI/Models/Validators/IndividualValidator.cs
@@ -0,0 +1,19 @@
+﻿using FluentValidation;
+using InSharpAssessment.WebAPI.Models.ViewModels;
+
+namespace InSharpAssessment.WebAPI.Models.Validators
+{
+    public class IndividualCreateValidator : AbstractValidator<IndividualCreateVM>
+    {
+        public IndividualCreateValidator()
+        {
+            RuleFor(x => x.FirstName)
+                .NotEmpty()
+                .WithMessage("First Name is required");
+
+            RuleFor(x => x.PhoneNumber)
+                .NotEmpty()
+                .WithMessage("Phone number is required");
+        }
+    }
+}
