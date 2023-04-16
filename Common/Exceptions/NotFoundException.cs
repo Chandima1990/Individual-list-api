8d9fec109f9c38c2e92a1c8468d4f10600fbaa85
Chandima Ekanayake <emcc1990@gmail.com>
2023-04-16 23:49:14 +0530

    introduced not found exception

---
+++ Common/Exceptions/NotFoundException.cs
@@ -0,0 +1,15 @@
+﻿using InSharpAssessment.Common.Exceptions;
+using System.Net;
+
+namespace Common.Exceptions
+{
+    public class NotFoundException : ApiException
+    {
+        public NotFoundException(HttpStatusCode httpStatusCode,
+            string message) :
+            base(httpStatusCode, message)
+        {
+
+        }
+    }
+}
