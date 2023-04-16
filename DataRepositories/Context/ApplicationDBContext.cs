4d938bdc791bff99b4cd77159139ec33cadfdd0d
Chandima Ekanayake <emcc1990@gmail.com>
2023-04-16 23:46:25 +0530

    data seeded

--- DataRepositories/Context/ApplicationDBContext.cs
+++ DataRepositories/Context/ApplicationDBContext.cs
@@ -7,11 +7,13 @@ namespace InSharpAssessment.DataRepositories.Context
     {
         public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options)
             : base(options)
-        { }
+        {
+            Database.EnsureCreatedAsync();
+        }
 
         #region DBSets
-        public DbSet<Individual> Individuals { get; set; } = null!;
-        public DbSet<Address> Addresses { get; set; } = null!;
+        public DbSet<Individual> Individuals { get; set; }
+        public DbSet<Address> Addresses { get; set; }
 
         #endregion
 
@@ -58,37 +60,27 @@ namespace InSharpAssessment.DataRepositories.Context
         #region Data Seeding
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            // Seed data for Individual entity
+            // Configure seed data for 'Individual' entity
             modelBuilder.Entity<Individual>().HasData(
-                new Individual
-                {
-                    Id = 1,
-                    FirstName = "John",
-                    LastName = "Doe",
-                    Addresses = new List<Address>
-                    {
-                        new Address { Id = 1, Street = "123 Main St", City = "New York", Country = "USA" },
-                        new Address { Id = 2, Street = "456 Elm St", City = "Los Angeles", Country = "USA" }
-                    },
-                    PhoneNumber = "555-1234",
-                    AgeInYears = 30
-                },
-                new Individual
-                {
-                    Id = 2,
-                    FirstName = "Jane",
-                    LastName = "Smith",
-                    Addresses = new List<Address>
-                    {
-                        new Address { Id = 3, Street = "789 Oak St", City = "Chicago", Country = "USA" },
-                        new Address { Id = 4, Street = "101 Maple St", City = "San Francisco", Country = "USA" }
-                    },
-                    PhoneNumber = "555-5678",
-                    AgeInYears = 25
-                }
+                new Individual { Id = 1, FirstName = "John", LastName = "Doe", PhoneNumber = "555-1234", AgeInYears = 30 },
+                new Individual { Id = 2, FirstName = "Jane", LastName = "Pifer", PhoneNumber = "555-5678", AgeInYears = 28 }
             );
-        }
 
+            // Configure seed data for 'Address' entity
+            modelBuilder.Entity<Address>().HasData(
+                new Address { Id = 1, Street = "123 Main St", City = "New York", Country = "USA", IndividualId = 1 },
+                new Address { Id = 2, Street = "456 Elm St", City = "Los Angeles", Country = "USA", IndividualId = 1 },
+                new Address { Id = 3, Street = "789 Oak St", City = "Chicago", Country = "USA", IndividualId = 2 }
+            );
+
+            // Define relationships between entities
+            modelBuilder.Entity<Individual>()
+                .HasMany(i => i.Addresses)
+                .WithOne(a => a.Individual)
+                .HasForeignKey(a => a.IndividualId);
+
+            // Other modelBuilder configurations
+        }
         #endregion
     }
 }
